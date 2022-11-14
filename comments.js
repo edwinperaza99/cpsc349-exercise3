d5a6dc53c515dd610b932251be516195b92af7cd
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2022-11-14 11:38:55 -0800

    started working on exercise 3

--- comments.js
+++ comments.js
@@ -44,3 +44,44 @@ for (const detail of details) {
 
 const posts = await downloadPosts()
 console.log(posts)
+
+// doing some testing
+const art = document.createElem('article')
+art.data-post-id = 
+document.body.appendChild(art)
+
+const head = document.createElem('h2')
+head.textContent = 
+art.appendChild(head)
+
+const as = document.createElem ('aside')
+const spa = document.createElem('span')
+spa.textContent = 
+as.textContent = by 
+art.appendChild(as)
+
+const para = document.createElem('p')
+para.textContent = 
+art.appendChild(para)
+
+const det = document.createElem('details')
+
+const sum = document.createElem('summary')
+sum.textContent = "See what our readers had to say..."
+det.appendChild(sum)
+
+const sect = document.createElem('section')
+det.appendChild(sect)
+
+const headsect = document.createElem('header')
+det.appendChild(headsect)
+
+const header3 = document.createElem('h3')
+header3.textContent = 
+headset.appendChild(header3)
+
+const asides = documet.createElem('aside')
+const para1 = document.createElem('p')
+asides.appendChild(para1)
+const para2 = document.createElem('p')
+asides.appendChild(para2)
