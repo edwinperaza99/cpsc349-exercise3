f7cc9ae63de5fd9107b9c3d44eb0bb43d4027f5c
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2022-11-16 00:11:54 -0800

    advanced on exercise 3

--- comments.js
+++ comments.js
@@ -1,90 +1,106 @@
 // https://jsonplaceholder.typicode.com/guide/
 
-async function downloadPosts (page = 1) {
-  const postsURL = `https://jsonplaceholder.typicode.com/posts?_page=${page}`
-  const response = await fetch(postsURL)
-  const articles = await response.json()
-  return articles
+async function downloadPosts(page = 1) {
+	const postsURL = `https://jsonplaceholder.typicode.com/posts?_page=${page}`;
+	const response = await fetch(postsURL);
+	const articles = await response.json();
+	return articles;
 }
 
-async function downloadComments (postId) {
-  const commentsURL = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
-  const response = await fetch(commentsURL)
-  const comments = await response.json()
-  return comments
+async function downloadComments(postId) {
+	const commentsURL = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+	const response = await fetch(commentsURL);
+	const comments = await response.json();
+	return comments;
 }
 
-async function getUserName (userId) {
-  const userURL = `https://jsonplaceholder.typicode.com/users/${userId}`
-  const response = await fetch(userURL)
-  const user = await response.json()
-  return user.name
+async function getUserName(userId) {
+	const userURL = `https://jsonplaceholder.typicode.com/users/${userId}`;
+	const response = await fetch(userURL);
+	const user = await response.json();
+	return user.name;
 }
 
-function getArticleId (comments) {
-  const article = comments.previousElementSibling
-  const data = article.dataset
-  return data.postId
+function getArticleId(comments) {
+	const article = comments.previousElementSibling;
+	const data = article.dataset;
+	return data.postId;
 }
 
-const details = document.getElementsByTagName('details')
+const details = document.getElementsByTagName("details");
 for (const detail of details) {
-  detail.addEventListener('toggle', async event => {
-    if (detail.open) {
-      const asides = detail.getElementsByTagName('aside')
-      const commentsWereDownloaded = asides.length > 0
-      if (!commentsWereDownloaded) {
-        const articleId = getArticleId(detail)
-        const comments = await downloadComments(articleId)
-        console.log(comments)
-      }
-    }
-  })
+	detail.addEventListener("toggle", async (event) => {
+		if (detail.open) {
+			const asides = detail.getElementsByTagName("aside");
+			const commentsWereDownloaded = asides.length > 0;
+			if (!commentsWereDownloaded) {
+				const articleId = getArticleId(detail);
+				const comments = await downloadComments(articleId);
+				console.log(comments);
+			}
+		}
+	});
 }
 
-const posts = await downloadPosts()
-console.log(posts)
+const posts = await downloadPosts();
+console.log(posts);
 
-// doing some testing
-const art = document.createElem('article')
-art.data-post-id = 
-main.appendChild(art)
+// function to replace new lines with br
+function newLine(para) {
+	para = para.replace(/(?:\n)/g, "<br>");
+	return para;
+}
 
-const head = document.createElem('h2')
-head.textContent = 
-art.appendChild(head)
+// actual code
+async function fillElements(obj) {
+	const objects = obj;
+	for (const objective of objects) {
+		const myMain = document.querySelector("main");
+		const art = document.createElement("article");
+		art.setAttribute("data-post-id", objective.id);
+		myMain.appendChild(art);
+		const myH2 = document.createElement("h2");
+		myH2.textContent = objective.title;
+		art.appendChild(myH2);
+		const myAside = document.createElement("aside");
+		// add span for the author name
+		const mySpan = document.createElement("span");
+		mySpan.setAttribute("class", "author");
+		mySpan.textContent = await getUserName(objective.userId);
+		myAside.textContent = `by `;
+		myAside.appendChild(mySpan);
+		art.appendChild(myAside);
+		// create paragraph
+		const myPara = document.createElement("p");
+		let myBody = objective.body;
+		myPara.innerHTML = newLine(myBody);
+		art.appendChild(myPara);
+		// add detail section
+		const det = document.createElement("details");
+		myMain.appendChild(det);
+		const mySum = document.createElement("summary");
+		mySum.textContent = "See what our readers had to say...";
+		det.appendChild(mySum);
+		const mySec = document.createElement("section");
+		det.appendChild(mySec);
+		const myHeader = document.createElement("header");
+		mySec.appendChild(myHeader);
+		const myH3 = document.createElement("h3");
+		myH3.textContent = "Comments";
+		myHeader.appendChild(myH3);
 
-const as = document.createElem ('aside')
-const spa = document.createElem('span')
-spa.textContent = 
-as.textContent = by 
-art.appendChild(as)
-
-const para = document.createElem('p')
-para.textContent = 
-art.appendChild(para)
-
-const det = document.createElem('details')
-main.appendChild(det)
-// attempting to add toggle attribute
-det.toggleAttribute('hiden')
-
-const sum = document.createElem('summary')
-sum.textContent = "See what our readers had to say..."
-det.appendChild(sum)
-
-const sect = document.createElem('section')
-det.appendChild(sect)
-
-const headsect = document.createElem('header')
-det.appendChild(headsect)
-
-const header3 = document.createElem('h3')
-header3.textContent = 
-headset.appendChild(header3)
-
-// const asides = documet.createElem('aside')
-// const para1 = document.createElem('p')
-// asides.appendChild(para1)
-// const para2 = document.createElem('p')
-// asides.appendChild(para2)
+		// add event listener
+		det.addEventListener("toggle", async (event) => {
+			if (det.open) {
+				const asides = det.getElementsByTagName("aside");
+				const commentsWereDownloaded = asides.length > 0;
+				if (!commentsWereDownloaded) {
+					const articleId = getArticleId(det);
+					const comments = await downloadComments(articleId);
+					console.log(comments);
+				}
+			}
+		});
+	}
+}
+fillElements(posts);
